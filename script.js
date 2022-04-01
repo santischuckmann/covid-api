2d2899de276ff840802a8c348be24766f492fd72
Santiago <schuckmannsantiago@gmail.com>
2022-03-31 22:23:00 -0300

    my first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+window.onload = iniciar;
+
+function iniciar () {
+  let boton = document.getElementById("btnCargar");
+  boton.addEventListener("click", clickBoton);
+}
+
+async function cargarUrl(url) {
+  let respuesta = await fetch(url);
+  return respuesta.json();
+}
+
+async function clickBoton () {
+  let pais = document.getElementById("selectPais").value;
+  let fecha = document.getElementById("inputFecha").value;
+  let url = `https://api.covid19tracking.narrativa.com/api/${fecha}/country/${pais}`
+  let json = await cargarUrl(url);
+  let estadisticas = json.dates[fecha].countries[pais];
+  console.log(estadisticas);
+
+  document.getElementById("today_confirmed").innerHTML = estadisticas.today_confirmed;
+  document.getElementById("today_deaths").innerHTML = estadisticas.today_deaths;
+  document.getElementById("today_hospitalised_patients_with_symptoms").innerHTML = estadisticas.today_hospitalised_patients_with_symptoms;
+  document.getElementById("today_intensive_care").innerHTML = estadisticas.today_intensive_care;
+  document.getElementById("today_new_confirmed").innerHTML = estadisticas.today_new_confirmed;
+  document.getElementById("today_new_deaths").innerHTML = estadisticas.today_new_deaths;
+  document.getElementById("today_new_hospitalised_patients_with_symptoms").innerHTML = estadisticas.today_new_hospitalised_patients_with_symptoms;
+  document.getElementById("today_new_intensive_care").innerHTML = estadisticas.today_new_intensive_care;
+  document.getElementById("today_new_open_cases").innerHTML = estadisticas.today_new_open_cases;
+  document.getElementById("today_new_recovered").innerHTML = estadisticas.today_new_recovered;
+  document.getElementById("today_new_total_hospitalised_patients").innerHTML = estadisticas.today_new_total_hospitalised_patients;
+
+}
